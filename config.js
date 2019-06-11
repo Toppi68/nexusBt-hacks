ab5bb13fb9e77bb819dcefa0eaedb7eea8c348a7
Toppi <toppi@04207.com>
2019-06-11 20:29:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ module.exports = class config {
                 }
             } else {
                 if (this.fs.existsSync(this.path)){
-                    this.fs.writeFile(this.path+this.fileName, "{/*config file*/}", (err) => {
+                    this.fs.writeFile(this.path+this.fileName, "{}", (err) => {
                         if (err) throw err;
                     });
                 } else {
